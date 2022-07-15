afd04318e2d7f3557f02e4bb52db009419ee579c
Hind ALLA <iamhiindou@gmail.com>
2022-07-15 22:53:23 +0100

    Error Fixed

--- youtube-clone-Backend/src/main/java/com/alla/youtubeclone/controllers/VideoController.java
+++ youtube-clone-Backend/src/main/java/com/alla/youtubeclone/controllers/VideoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/videos")
+@CrossOrigin("*")
 @RequiredArgsConstructor
 public class VideoController {
 
